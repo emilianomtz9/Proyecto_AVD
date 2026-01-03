d612a5d37055bf6bbc8e38721f158bb83df397e7
emilianomtz9 <133317338+emilianomtz9@users.noreply.github.com>
2026-01-02 22:00:52 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -174,9 +174,10 @@ with divs[1]:
 with divs[2]:
     st.subheader("Correlación entre contaminantes")
     corr = df_f[seleccionados].corr()
-    fig = px.imshow(corr,aspect="auto",zmin=-1, zmax=1, origin="lower",text_auto=".2f")
+    fig = px.imshow(corr,aspect="auto",zmin=-1, zmax=1 ,text_auto=".2f")
     fig.update_layout(title="Matriz de correlación",coloraxis_colorbar=dict(title="r", tickvals=[-1, -0.5, 0, 0.5, 1]))
     st.plotly_chart(fig, use_container_width=True)
+    fig.update_yaxes(autorange="reversed")
 
 #Normalización con z
 x= df_f[["fecha"]+seleccionados].set_index("fecha").dropna()
